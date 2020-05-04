a667110df62c2a376bcf6039b83552d54bd0d782
Micah McCallum <micahmccallum@outlook.com>
2020-05-04 04:50:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,6 +20,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student_hash = {}
     link_array = []
+    blog_link = nil
     social_networks = ["twitter", "github", "linkedin"]
     doc = get_doc(profile_url)
     social_icon_container = doc.css(".social-icon-container")
@@ -28,19 +29,17 @@ class Scraper
     image_urls = image_links.map {|link| link[:src]}
     
     links.each do |link|
+      blog_link = link[:href] if link.css("img").first["src"].include?("../assets/img/rss-icon.png")
       link_array << link[:href]
     end
     
     link_array.each do |link|
-      binding.pry
       social_networks.each do |network|
         student_hash[network.to_sym] =  link if link.include?(network)
       end
     end
-    
-    binding.pry
    
-    student_hash[:blog] = links if image_urls.include?("../assets/img/rss-icon.png")
+    student_hash[:blog] = blog_link if blog_link
     student_hash[:profile_quote] = doc.css(".profile-quote").text
     student_hash[:bio] = doc.css(".bio-content p").text
     student_hash
