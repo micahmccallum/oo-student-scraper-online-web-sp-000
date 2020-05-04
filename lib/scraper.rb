5244f009ebc491cd5d1a62c75339f39a2e37daa1
Micah McCallum <micahmccallum@outlook.com>
2020-05-04 02:49:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,27 +18,31 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    student_hash = {}
+    link_array = []
+    social_networks = ["twitter", "github", "linkedin"]
     doc = get_doc(profile_url)
-    social_icons = doc.css(".social-icon-container")
+    social_icon_container = doc.css(".social-icon-container")
+    links = social_icon_container.xpath("//a")
+    image_links = links.xpath("//img")
+    image_urls = image_links.map {|link| link[:src]}
     
+    links.each do |link|
+      link_array << link[:href]
+    end
     
+    link_array.each do |link|
+      social_networks.each do |network|
+        student_hash[network.to_sym] =  link if link.include?(network)
+      end
+    end
     
-    
-    twitter = social_icons.css("a[href*='twitter']").first["href"] if social_icons.css("a[href*='twitter']").first["href"].include?("twitter")
-    linkedin = social_icons.css("a[href*='linkedin']").first["href"] if social_icons.css("a[href*='linkedin']") != []
-    github = social_icons.css("a[href*='github']").first["href"] if social_icons.css("a[href*='github']") != []
-    blog = social_icons.xpath("//a").last["href"]
-    profile_quote = doc.css(".profile-quote").text
-    bio = doc.css(".bio-content p").text
-    student_hash = {
-      :twitter => twitter,
-      :linkedin => linkedin,
-      :github => github,
-      :blog => blog,
-      :profile_quote => profile_quote,
-      :bio => bio
-    }
     binding.pry
+   
+    student_hash[:blog] = links if image_urls.include?("../assets/img/rss-icon.png")
+    student_hash[:profile_quote] = doc.css(".profile-quote").text
+    student_hash[:bio] = doc.css(".bio-content p").text
+    student_hash
   end
   
   def self.get_doc(url)
